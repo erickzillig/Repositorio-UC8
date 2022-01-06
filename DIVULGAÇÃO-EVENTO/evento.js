ca9c0c4023191d7a610fe9ab1105081525472106
erickzillig <erickzillig1029@gmail.com>
2022-01-06 15:12:52 -0200

    Divulgaçao de Evento

---
+++ DIVULGAÇÃO-EVENTO/evento.js
@@ -0,0 +1,21 @@
+const data = new data ()
+const datadoevento = 10/05/2021
+if (datadoevento <= new data){
+    console.log ("Evento Permitido")
+} else {
+    console.log ("Evento não permitido, selecione uma nova data")
+}
+
+let idade = 23
+if (idade >= 18){
+    console.log ("Cadastro valido")
+} else {
+    console.log ("Idade invalida, tente novamente!")
+}
+
+let listadeparticipantes = [ "Pedro", "Julia", "Eduarda"]
+if (ListadePeça.length < 100) {
+    console.log ("É possivel cadastrar mais participantes")
+} else {
+    console.log ("Não tem mais espaço na lista")
+}
